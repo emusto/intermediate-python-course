8628cdc0234731de4dfd3c9c3eb347d24444aa9b
emusto <elisa.musto@gmail.com>
2020-05-25 15:04:08 +0200

    I added a loop to roll two dice and a conditional comment on the rool

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,18 @@
 import random
 
 def main():
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    if roll == 1:
+      print(f'You rolled a {roll}! Critical Fail')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Critical Success!')
+    else:
+      print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
